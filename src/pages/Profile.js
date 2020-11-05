7b26fe6ef4d2f59c5dec914941cd85be95cc2673
EzFroze <sultanbekov-01@yandex.ru>
2020-11-05 05:56:54 +0300

    Profile component for current user

---
+++ src/pages/Profile.js
@@ -0,0 +1,72 @@
+import React, {Fragment, useContext, useEffect} from "react"
+import {GithubContext} from "../context/Github/githubContext";
+import {Link} from "react-router-dom";
+import {Repos} from "../components/Repos";
+
+export const Profile = ({match}) => {
+  const {getUser, getRepos, loading, user, repos} = useContext(GithubContext)
+  const url_name = match.params.name
+
+  useEffect(() => {
+    getUser(url_name)
+    getRepos(url_name)
+    // eslint-disable-next-line
+  }, [])
+
+  if (loading) {
+    return <p className="text-center">Загрузка...</p>
+  }
+
+  const {
+    name, company, avatar_url,
+    location, blog, login, html_url, followers,
+    following, public_repos, public_gists, bio
+  } = user
+
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-link">На главную</Link>
+
+      <div className="card mb-4">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-3 text-center">
+              <img src={avatar_url} alt={name} style={{width: "200px"}}/>
+              <h1>{name}</h1>
+              {location && <p>Местоположение: {location}</p>}
+            </div>
+            <div className="col">
+              {
+                bio && <Fragment>
+                  <h3>BIO</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              }
+              <a href={html_url} className="btn btn-dark" target="_blank" rel="noopener noreferrer">Открыть профиль</a>
+              <ul>
+                {login && <li>
+                  <strong>Username: </strong> {login}
+                </li>}
+
+                {company && <li>
+                  <strong>Компания: </strong> {company}
+                </li>}
+
+                {blog && <li>
+                  <strong>Website: </strong> {blog}
+                </li>}
+              </ul>
+
+              <div className="badge badge-primary">Подписчики: {followers}</div>
+              <div className="badge badge-success">Подписан: {following}</div>
+              <div className="badge badge-info">Репозитории: {public_repos}</div>
+              <div className="badge badge-dark">Gists: {public_gists}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Repos repos={repos}/>
+    </Fragment>
+  )
+}
