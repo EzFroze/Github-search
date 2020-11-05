12c335e915839dbd218e164954c0549abd24da6c
EzFroze <sultanbekov-01@yandex.ru>
2020-11-05 05:57:49 +0300

    Component for search users

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import React, {useContext, useState} from "react"
+import {AlertContext} from "../context/Alert/alertContext";
+import {GithubContext} from "../context/Github/githubContext";
+
+export const Search = () => {
+  const [value, setValue] = useState("")
+  const {show, hide} = useContext(AlertContext)
+  const github = useContext(GithubContext)
+
+  const onSubmit = event => {
+    if (event.key !== "Enter") {
+      return
+    }
+
+    github.clearUsers()
+
+    if (value.trim()) {
+      github.search(value.trim())
+      hide()
+    } else {
+      show("Введите данные пользователя!")
+    }
+  }
+  return (
+    <div className="form-group">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Введите ник пользователя..."
+        onKeyPress={onSubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+    </div>
+  )
+}
